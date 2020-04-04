38b4b88b186a0e2e4734cb9410561ca8c148f6ec
velooor <velooor@gmail.com>
2020-04-04 10:23:03 +0300

    53. Maximum Subarray

--- src/main/java/com/leetcode/vadim/problem/challenge/ChallengeSolution.java
+++ src/main/java/com/leetcode/vadim/problem/challenge/ChallengeSolution.java
@@ -75,11 +75,26 @@ public class ChallengeSolution {
     }
 
 
+    // 53. Maximum Subarray
+    // https://leetcode.com/problems/maximum-subarray/
+    public static int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
+        dp[0] = nums[0];
+        int max = dp[0];
 
+        for(int i = 1; i < n; i++){
+            dp[i] = nums[i] + (Math.max(dp[i - 1], 0));
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
 
 
     public static void main(String[] args) {
-        System.out.println(isHappy1(19));
+        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        //System.out.println(isHappy1(19));
         //System.out.println(singleNumber(new int[]{2, 2, 1}));
     }
 }
