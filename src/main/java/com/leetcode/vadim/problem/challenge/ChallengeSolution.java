7a8e84c392a35733f13c98efdbbc54c416d357fc
velooor <velooor@gmail.com>
2020-04-04 11:41:23 +0300

    283. Move Zeroes

--- src/main/java/com/leetcode/vadim/problem/challenge/ChallengeSolution.java
+++ src/main/java/com/leetcode/vadim/problem/challenge/ChallengeSolution.java
@@ -2,6 +2,7 @@ package com.leetcode.vadim.problem.challenge;
 
 import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -83,7 +84,7 @@ public class ChallengeSolution {
         dp[0] = nums[0];
         int max = dp[0];
 
-        for(int i = 1; i < n; i++){
+        for (int i = 1; i < n; i++) {
             dp[i] = nums[i] + (Math.max(dp[i - 1], 0));
             max = Math.max(max, dp[i]);
         }
@@ -91,9 +92,80 @@ public class ChallengeSolution {
         return max;
     }
 
+    // 283. Move Zeroes
+    // https://leetcode.com/problems/move-zeroes/
+    public static void moveZeroes(int[] nums) {
+        /*int z = 0, nz = 0;
+        while (nz < nums.length - 1) {
+            while (z < nums.length-1) if (nums[z] == 0) break; else z++;
+            while (nz < nums.length-1) if (nums[nz] != 0) break; else nz++;
+            if(z > nz) { nz++; continue; }
+            nums[z] = nums[nz]; nums[nz] = 0;
+        }*/
+
+
+        if (nums == null || nums.length == 0) return;
+
+        int insertPos = 0;
+        for (int num: nums) {
+            if (num != 0) nums[insertPos++] = num;
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        int[] res;
+
+        res = new int[]{1, 0, 2};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+        res = new int[]{0, 1, 0};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+        res = new int[]{1, 0, 0};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+        res = new int[]{0, 1};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+        res = new int[]{1, 0};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+        res = new int[]{1, 2, 3};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+        res = new int[]{0, 1, 0, 3, 12};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+        res = new int[]{0};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+        res = new int[]{1};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+
+        res = new int[]{0, 0, 0};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+        res = new int[]{0, 0, 1, 0, 0, 2, 3};
+        moveZeroes(res);
+        System.out.println(Arrays.toString(res));
+
+        //System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
         //System.out.println(isHappy1(19));
         //System.out.println(singleNumber(new int[]{2, 2, 1}));
     }
