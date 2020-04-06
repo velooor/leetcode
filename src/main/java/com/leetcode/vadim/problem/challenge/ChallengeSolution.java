f915afc5e13f4c9f6d921d26f0200d51d6434e52
velooor <velooor@gmail.com>
2020-04-06 12:31:41 +0300

    49. Group Anagrams

--- src/main/java/com/leetcode/vadim/problem/challenge/ChallengeSolution.java
+++ src/main/java/com/leetcode/vadim/problem/challenge/ChallengeSolution.java
@@ -149,6 +149,21 @@ public class ChallengeSolution {
     }
 
 
+    // 49. Group Anagrams
+    // https://leetcode.com/problems/group-anagrams/
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> ans = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+            ans.get(key).add(s);
+        }
+        return new ArrayList<>(ans.values());
+    }
+
     public static void main(String[] args) {
         System.out.println(7 == maxProfit(new int[]{7, 1, 5, 3, 6, 4})); // 7
         System.out.println(3 == maxProfit(new int[]{1, 2, 3, 4})); // 3
