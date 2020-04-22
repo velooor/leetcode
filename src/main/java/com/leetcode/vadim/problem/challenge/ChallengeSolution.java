c051f6b92c0c17b67cea8c2eef6f7010a4960e47
velooor <velooor@gmail.com>
2020-04-22 22:45:15 +0300

    560. Subarray Sum Equals K

--- src/main/java/com/leetcode/vadim/problem/challenge/ChallengeSolution.java
+++ src/main/java/com/leetcode/vadim/problem/challenge/ChallengeSolution.java
@@ -441,6 +441,24 @@ public class ChallengeSolution {
         return j==binaryMatrix.dimensions().get(1)-1?-1:j+1;
     }*/
 
+
+    // 560. Subarray Sum Equals K
+    // https://leetcode.com/problems/subarray-sum-equals-k/
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i <= nums.length; i++)
+            sum[i] = sum[i - 1] + nums[i - 1];
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                if (sum[end] - sum[start] == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         System.out.println(7 == maxProfit(new int[]{7, 1, 5, 3, 6, 4})); // 7
         System.out.println(3 == maxProfit(new int[]{1, 2, 3, 4})); // 3
